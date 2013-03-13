ef76890ac461d4119b9beea9d89e8b37f2e46737
deepwinter <deepwinter@winterroot.net>
2013-03-13 15:59:18 -0700

    made the card view table cell

---
+++ HPDT_CardTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  HPDT_CardTableViewCell.h
+//  Simpler
+//
+//  Created by Snacks on 3/13/13.
+//  Copyright (c) 2013 HPDTApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HPDT_CardTableViewCell : UIView
+
+@end
