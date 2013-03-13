e7b300a0db7453108f374d8ca54a1a23ffbaf7be
deepwinter <deepwinter@winterroot.net>
2013-03-12 19:47:38 -0700

    made all the black xib and controller files

---
+++ Simpler/HPDT_AddCreditViewController.h
@@ -0,0 +1,13 @@
+//
+//  HPDT_AddCreditViewController.h
+//  Simpler
+//
+//  Created by Snacks on 3/12/13.
+//  Copyright (c) 2013 HPDTApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HPDT_AddCreditViewController : UIViewController
+
+@end
