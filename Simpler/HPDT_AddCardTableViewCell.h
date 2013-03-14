533038ec3e0625efa34d7c55bf87985eb70e78c3
deepwinter <deepwinter@winterroot.net>
2013-03-13 18:12:32 -0700

    added the cells for adding a card

---
+++ Simpler/HPDT_AddCardTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  HPDT_AddCardTableViewCell.h
+//  Simpler
+//
+//  Created by Snacks on 3/13/13.
+//  Copyright (c) 2013 HPDTApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HPDT_AddCardTableViewCell : UIView
+
+@end
