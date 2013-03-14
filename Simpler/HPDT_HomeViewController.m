72c9e89b288611c5d5192cb7850f5bda05112279
deepwinter <deepwinter@winterroot.net>
2013-03-13 17:23:13 -0700

    Home view

--- Simpler/HPDT_HomeViewController.m
+++ Simpler/HPDT_HomeViewController.m
@@ -33,6 +33,8 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    self.navigationController.navigationBarHidden = YES;
 }
 
 - (void)viewDidUnload
